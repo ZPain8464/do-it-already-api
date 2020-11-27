fae8ec4a5bb90ec45c1b7e7bce40a91f4cefed9f
Zach Painter <z.painter45@gmail.com>
2020-11-27 17:47:46 -0600

    updated config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   API_TOKEN: process.env.API_TOKEN,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
-  DATABASE_URL:
-    process.env.DATABASE_URL || "postgresql://apple@localhost/do_it_already",
+  DATABASE_URL: process.env.DATABASE_URL || TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "do-it-already-jwt",
 };
