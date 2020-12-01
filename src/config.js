4956178747dd4e77d8132497c944d9788ed4ea35
Zach Painter <z.painter45@gmail.com>
2020-12-01 14:38:52 -0600

    edited config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   API_TOKEN: process.env.API_TOKEN,
-  CLIENT_ORIGIN: "http://localhost:3000/login",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
   DATABASE_URL: process.env.DATABASE_URL || TEST_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || "do-it-already-jwt",
 };
